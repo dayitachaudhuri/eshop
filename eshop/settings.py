0346d26c85a3869c7825c78137bcacf41e19fa76
dayita <dayitachaudhuri.tech@gmail.com>
2023-11-06 02:19:17 +0530

    Render

--- eshop/settings.py
+++ eshop/settings.py
@@ -144,12 +144,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
